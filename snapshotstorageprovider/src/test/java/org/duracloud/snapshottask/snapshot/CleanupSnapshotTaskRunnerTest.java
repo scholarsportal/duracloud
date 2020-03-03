d6c3d77e62556482bb82cc6dcd10c87ae8c31e00
Mohana Sarmiento <mo.sarmiento@utoronto.ca>
2020-03-03 14:43:08 -0500

    Remove commented line in SnapshotStorageProvider.
    Changes to unit tests for snapshot tasks.

--- snapshotstorageprovider/src/test/java/org/duracloud/snapshottask/snapshot/CleanupSnapshotTaskRunnerTest.java
+++ snapshotstorageprovider/src/test/java/org/duracloud/snapshottask/snapshot/CleanupSnapshotTaskRunnerTest.java
@@ -119,7 +119,7 @@ public class CleanupSnapshotTaskRunnerTest extends EasyMockSupport {
         Capture<Set<Task>> taskCapture = Capture.newInstance(CaptureType.FIRST);
 
         expect(unwrappedSnapshotProvider.getStorageProviderType())
-            .andReturn(StorageProviderType.AMAZON_S3);
+            .andReturn(StorageProviderType.AMAZON_S3).times(2);
 
         auditQueue.put(capture(taskCapture));
         expectLastCall().once();
